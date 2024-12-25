2694fa95d55ed0b60283e6b82ff523a9609fc07f
Enes Birlik <enesbirlik0@gmail.com>
2024-12-25 17:29:36 +0300

    sildim gitti

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-from .core.robot_base import RobotBase
-from .core.robot_configs import RobotConfig, RobotConfigs
-from .solvers import CCDSolver, FABRIKSolver, JacobianSolver, DLSSolver
-
-__version__ = '0.1.0'
--- src/core/__init__.py
+++
@@ -1,3 +0,0 @@
-from .robot_base import RobotBase
-from .robot_configs import RobotConfigs, RobotConfig
-from .utils import *
